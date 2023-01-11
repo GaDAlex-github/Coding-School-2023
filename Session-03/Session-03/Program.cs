443b9205ee3c8c48a19936258e133d71416b614d
GaDAlex <alexaras1311@gmail.com>
2023-01-11 16:40:03 +0300

    add Session-03

--- Session-03/Session-03/Program.cs
+++ Session-03/Session-03/Program.cs
@@ -1,8 +1,3 @@
-﻿    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-        Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
