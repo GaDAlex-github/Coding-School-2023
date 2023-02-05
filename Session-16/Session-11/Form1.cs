22ed22dff26a7b69e0f13581c79b06d592bfdd46
GaDAlex <alexaras1311@gmail.com>
2023-02-05 15:26:36 +0300

    removed populates / added new ppl in db

--- Session-16/Session-11/Form1.cs
+++ Session-16/Session-11/Form1.cs
@@ -1,4 +1,5 @@
 using DevExpress.XtraGrid.Columns;
+using DevExpress.XtraGrid.Views.Grid;
 using EF.PetShop.Orm.Repositories;
 using Session_11.EF.PetShopModel;
 using Session_11.EF.PetShopOrm.Repositories;
@@ -27,25 +28,15 @@ namespace Session_11 {
 
         private void Form1_Load(object sender, EventArgs e) {
 
-
-            List<Customer> customers = new List<Customer>();
-
-            petShop = new PetShop();
             _petShopRepo = new PetShopRepo();
+            petShop = _petShopRepo.GetById(1);
             _customerRepo = new CustomerRepo();
             _employeeRepo = new EmployeeRepo();
             _petRepo = new PetRepo();
             _petFoodRepo = new PetFoodRepo();
             _transactionRepo = new TransactionRepo();
-
-            PopulateEmployee();
-            PopulateCustomers();
-            PopulatePets();
-            PopulatePetFoods();
             SetBindings();
             SetControls();
-
-            //PopulateLastChanges();
             //List<Transaction> transactions = petShop.Transactions;
             //monthlyLedgerReport.CreateMonthlyLedgerReport(transactions);
         }
@@ -77,103 +68,12 @@ namespace Session_11 {
 
             return summary;
         }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) {
-
-        }
-
-
-        public void PopulatePets() {
-
-            var pets = new List<Pet>
-            {
-                new Pet {ID=1,AnimalType = Pet.AnimalTypeEnum.Dog,Status=Pet.PetStatusEnum.OK ,Price = 100, Cost = 70,Breed="French Bulldog"},
-                new Pet {ID=2,AnimalType = Pet.AnimalTypeEnum.Dog,Status=Pet.PetStatusEnum.Recovering ,Price =80, Cost = 30,Breed="Akita"},
-                new Pet {ID=3,AnimalType = Pet.AnimalTypeEnum.Dog,Status=Pet.PetStatusEnum.Unhealthy ,Price =0, Cost = 20,Breed="Beagle"},
-                new Pet {ID=4,AnimalType = Pet.AnimalTypeEnum.Cat,Status=Pet.PetStatusEnum.OK, Price = 100, Cost = 80,Breed="Persian"},
-                new Pet {ID=5,AnimalType = Pet.AnimalTypeEnum.Cat,Status=Pet.PetStatusEnum.Recovering, Price = 80, Cost = 60,Breed="Maine Coon"},
-                new Pet {ID=6,AnimalType = Pet.AnimalTypeEnum.Cat,Status=Pet.PetStatusEnum.Unhealthy, Price = 0, Cost = 20,Breed="Ragdoll"},
-                new Pet {ID=7,AnimalType = Pet.AnimalTypeEnum.Bird,Status=Pet.PetStatusEnum.OK, Price = 80, Cost = 50,Breed="Canary"},
-                new Pet {ID=8,AnimalType = Pet.AnimalTypeEnum.Bird,Status=Pet.PetStatusEnum.Recovering, Price = 60, Cost = 40,Breed="Amazon Parrot"},
-                new Pet {ID=9,AnimalType = Pet.AnimalTypeEnum.Bird,Status=Pet.PetStatusEnum.Unhealthy, Price = 30, Cost = 15,Breed="African Grey Parrot"}
-            };
-            petShop.Pets.AddRange(pets);
-        }
-
-        public void PopulatePetFoods() {
-            var petsFood = new List<PetFood>
-            {
-                new PetFood {ID=1,AnimalType = PetFood.AnimalTypeEnum.Dog,PetFoodPrice = 9, PetFoodCost = 3},
-                new PetFood {ID=2,AnimalType = PetFood.AnimalTypeEnum.Cat,PetFoodPrice = 6, PetFoodCost = 2},
-                new PetFood {ID=3,AnimalType = PetFood.AnimalTypeEnum.Bird,PetFoodPrice = 3, PetFoodCost = 1},
-
-            };
-            petShop.PetFoods.AddRange(petsFood);
-        }
-        public void PopulateCustomers() {
-
-            Customer Cust1 = new Customer() {
-
-                Name = "Nikos",
-                Surname = "Karamitos",
-                Phone = "6978319532",
-                TIN = "37482910",
-
-            };
-            Customer cust2 = new Customer() {
-
-                Name = "Alex",
-                Surname = "Gad",
-                Phone = "6973132822",
-                TIN = "38239102"
-            };
-
-
-            petShop.Customers.Add(Cust1);
-            petShop.Customers.Add(cust2);
-
-        }
-
-        private void PopulateEmployee() {
-
-            Employee employee1 = new Employee() {
-                ID = 1,
-                Name = "Fotis",
-                Surname = "Chrysoulas",
-                EmployeeType = Employee.EmployeeTypeEnum.Manager,
-                SalaryPerMonth = 1000
-
-            };
-
-            Employee employee2 = new Employee() {
-                ID = 2,
-                Name = "Alex",
-                Surname = "Gad",
-                EmployeeType = Employee.EmployeeTypeEnum.Staff,
-                SalaryPerMonth = 1000
-
-            };
-            petShop.Employees.Add(employee1);
-            petShop.Employees.Add(employee2);
-            bsEmployee.DataSource = petShop.Employees;
-
-        }
-
         public void SetControls() {
 
-            GridColumn colCustomer = grvTransactions.Columns["colCustomerName"] as GridColumn;
-            //grvTransactions.CustomColumnDisplayText += GrvTransactions_CustomColumnDisplayText;
-            // colCustomer.DataSource = petShop.Customers;
-            //colCustomer.DisplayMember = "FullName";
-            // colCustomer.ValueMember = "CustomerID";
-            GridColumn colEmployee = grvTransactions.Columns["colEmployeeName"] as GridColumn;
-
-            // colEmployee.DataSource = petShop.Employees;
-            //  colEmployee.DisplayMember = "FullName";
-            // colEmployee.ValueMember = "EmployeeID";
-            GridColumn colAnimalType = grvTransactions.Columns["colPetID"] as GridColumn;
-            //  colAnimalType.DataSource = petShop.Pets;
-            // colAnimalType.DisplayMember = "AnimalBreed";
+            //GridColumn colCustomer = grvTransactions.Columns["colCustomerName"] as GridColumn;            
+            //GridColumn colEmployee = grvTransactions.Columns["colEmployeeName"] as GridColumn;
+            //GridColumn colAnimalType = grvTransactions.Columns["colPetID"] as GridColumn;
+            
         }
 
         //private void GrvTransactions_CustomColumnDisplayText(object sender, DevExpress.XtraGrid.Views.Base.CustomColumnDisplayTextEventArgs e) {
@@ -193,58 +93,28 @@ namespace Session_11 {
             bsTransaction.DataSource = petShop.Transactions;
             //   monthlyLedgerBindingSource.DataSource = petShop.MonthlyLedgers;
         }
-        
-        private void button1_Click(object sender, EventArgs e) {
-            Transaction tras = new Transaction();
-            bsTransaction.Add(tras);
-        }
 
-        private void btnRemoveTrans_Click(object sender, EventArgs e) {
-            bsTransaction.RemoveCurrent();
-        }
-
-        private void btnAddEmployee_Click(object sender, EventArgs e) {
-            Employee newEmployee = new Employee();
-            bsEmployee.Add(newEmployee);
-        }
-
-        private void btnRemoveEmployee_Click(object sender, EventArgs e) {
-            bsEmployee.RemoveCurrent();
-        }
-               
-        private void btnAddPet_Click(object sender, EventArgs e) {
-            Pet pet = new Pet();
-            bsPet.Add(pet);
-        }
-
-        private void button2_Click(object sender, EventArgs e) {
-            PetFood petFood = new PetFood();
-            bsPetFood.Add(petFood);
-
-        }       
-
-        private void btnAddCustomer_Click(object sender, EventArgs e) {
-            Customer newCust = new Customer();
-            bsCustomer.Add(newCust);
-        }
 
         private void btnLoad_Click_1(object sender, EventArgs e) {
             //Serializer serializer = new Serializer();
             //petShop = serializer.Deserialize<PetShop>("PetShop.json");
-            SetBindings();
-            grcCustomers.DataSource = _customerRepo.GetAll();
-            grcEmployees.DataSource = _employeeRepo.GetAll();
-            grcPets.DataSource = _petRepo.GetAll();
-            grcPetFoods.DataSource = _petFoodRepo.GetAll();
-            grcTransactions.DataSource = _transactionRepo.GetAll();
+            //SetBindings();
+            //grcCustomers.DataSource = _customerRepo.GetAll();
+            //grcEmployees.DataSource = _employeeRepo.GetAll();
+            //grcPets.DataSource = _petRepo.GetAll();
+            //grcPetFoods.DataSource = _petFoodRepo.GetAll();
+            //grcTransactions.DataSource = _transactionRepo.GetAll();
 
             MessageBox.Show("Data Load Correctly!");
         }
 
         private void btnSave_Click_1(object sender, EventArgs e) {
-            Serializer serializer = new Serializer();
-            serializer.SerializeToFile(petShop, "PetShop.json");
-            _petShopRepo.Add(petShop);
+            //Serializer serializer = new Serializer();
+            //serializer.SerializeToFile(petShop, "PetShop.json");
+            if (petShop == null)
+                _petShopRepo.Add(petShop);
+            else
+                _petShopRepo.Update(1, petShop);
         }
     }
 }
