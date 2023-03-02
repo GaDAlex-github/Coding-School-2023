88aad4050d55f464825f434425c230d8a436ad03
GaDAlex <alexaras1311@gmail.com>
2023-03-02 14:17:49 +0300

    fixed that shit that made me nervous

--- Session-30/FuelStation/FuelStation.EF/Repositories/ITransactionLineRepo.cs
+++ Session-30/FuelStation/FuelStation.EF/Repositories/ITransactionLineRepo.cs
@@ -1,11 +1,7 @@
 ﻿namespace FuelStation.EF.Repositories {
-    public interface ITransactionLineRepo<TEntity> {
-        IList<TEntity> GetAll();
-        TEntity? GetById(int id);
-        IList<TEntity> GetAllWithTransactionID(int id);
-        void Add(TEntity entity);
-        void Update(int id, TEntity entity);
-        void Delete(int id);
+    public interface ITransactionLineRepo<TEntity> : IEntityRepo<TEntity>{
+            IList<TEntity> GetAllWithTransactionID(int id);
+     
     }
 }
 
